8592adbd7d860f24ad7269c9648d300bbd9d7db2
Its-suLav-D <dah19001@byui.edu>
2020-05-16 11:47:44 -0700

    Lesson4 Assigment commited

---
+++ lesson4/app.js
@@ -0,0 +1,40 @@
+
+let mainNav = document.getElementById('menu');
+let navToggle = document.getElementById('toggleMenu');
+
+navToggle.addEventListener('click', function() {
+  mainNav.classList.toggle('popUp');
+})
+
+
+let daysNames = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday'
+];
+let months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December'
+];
+
+let date = new Date();
+let dayName = daysNames[date.getDay()];
+let monthName = months[date.getMonth()];
+let year = date.getFullYear();
+let fullDate = dayName + ', '+ date.getDate() +" " + monthName + " "+ year;
+
+document.getElementById('lastUpdated').textContent = fullDate;
