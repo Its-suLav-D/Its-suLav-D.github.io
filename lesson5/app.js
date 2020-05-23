7aa0356b39ed3629484a0a372f961251ccf73521
Its-suLav-D <dah19001@byui.edu>
2020-05-22 23:14:30 -0700

    minor error

--- lesson5/app.js
+++ lesson5/app.js
@@ -64,11 +64,11 @@ function checkAd() {
   let ul = document.querySelector('ul');
   let li = document.querySelectorAll('li');
 
-  li.forEach(el => {
-    el.addEventListener('click', function() {
+  li.forEach(item => {
+    item.addEventListener('click', function() {
       ul.querySelector('.active').classList.remove('active');
 
-      el.classList.add('active');
+      item.classList.add('active');
     });
   });
 }
