2de56aba7c89b9f11462f90442bd2fa552f59662
Its-suLav-D <dah19001@byui.edu>
2020-06-16 13:29:20 -0700

    week 9 uploaded

---
+++ lesson9/prophets.js
@@ -0,0 +1,41 @@
+const url = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+
+fetch(url)
+.then(function(response) {
+    if(response.ok) {
+        return response.json();
+    }
+   throw new ERROR('Network response was not ok'); 
+})
+.then(function(jsonObject){
+    console.table(jsonObject); // temporary checking for valid response and data parsing 
+    const prophets = jsonObject['prophets'];
+    prophets.forEach(prophet => {
+        let card = document.createElement('section');
+        let h2 = document.createElement('h2');
+        h2.classList.add('prophet-title');
+        let dob = document.createElement('p');
+        dob.classList.add('prophet-info');
+        let pob = document.createElement('p');
+        pob.classList.add('prophet-infos');
+        let image = document.createElement('img');
+        
+
+        h2.textContent = prophet.name+ ' ' + prophet.lastname;
+        dob.textContent = 'Date of Birth: ' + prophet.birthdate;
+        pob.textContent = 'Place of Birth: ' + prophet.birthplace;
+        image.setAttribute('src',prophet.imageurl);
+        card.append(h2);
+        card.append(dob);
+        card.append(pob);
+        card.append(image);
+        document.querySelector('div.cards').appendChild(card);
+       
+})
+})
+.catch(function(error){
+    console.log('Fetch error: ', error.message);
+});
+ 
+
+
